02969d375dcb9d5996998e6039095d5642a049b2
diyana-mitkova <didimitkova1@gmail.com>
2024-06-01 19:34:06 +0300

    add header image and creating a textbox component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,17 @@
-
+import Image from "next/image";
+import TextBox from "../components/text-box/TextBox";
 
 export default function Home() {
   return (
-  <div>Home Page</div>
+    <div className="flex flex-col gap-10">
+      <Image
+        width={1200}
+        height={620}
+        alt="header image"
+        src="/header.webp"
+        className="rounded-xl"
+      />
+      <TextBox />
+    </div>
   );
 }
